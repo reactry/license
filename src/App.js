552a5ead6de81d70c2d0cb5a87dcfcf12587bb11
Ankur Seth <iankurseth@gmail.com>
2022-01-15 22:01:28 +0530

    c0 with HW

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-400 text-white p-4 text-center">
+        <p className="text-2xl py-12">
+          Hallo Welt.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
