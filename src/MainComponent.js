2ce9e80ab5be357703ce8aec7131952bd28e1abd
Ankur Seth <iankurseth@gmail.com>
2022-01-15 23:50:59 +0530

    Helping the Americans 2.0

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-import Licences from './Licences';
+import Licenses from './Licenses';
 
-import licences_json from './licences.json';
-const licences = licences_json.licences;
+import licenses_json from './licenses.json';
+const licenses = licenses_json.licenses;
 
 export default function MainComponent () {
-	let licences_option_items = licences.map((licence) =>
-		<option value={licence.name}>{licence.title}</option>
+	let licenses_option_items = licenses.map((license) =>
+		<option value={license.name}>{license.title}</option>
 	);
 
 	return (
 		<main className="bg-slate-200 py-12">
 			<div className="max-w-lg m-auto bg-white min-h-screen p-8">
-				<h3 className="p-3 text-center text-xl">Choose your Licence</h3>
+				<h3 className="p-3 text-center text-xl">Choose your License</h3>
 				<form>
-					<select name="licence_name">
-						{licences_option_items}
+					<select name="license_name">
+						{licenses_option_items}
 					</select>
 				</form>
 			</div>
