70913bfb60fdf6ee2263505ac708e51b6d5db364
Ankur Seth <iankurseth@gmail.com>
2022-01-16 21:52:30 +0530

    Updated header * added link to self *

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,9 +1,12 @@
 
 export default function HeaderComponent () {
 	return (
-		<header className="bg-blue-600 text-white p-4 text-center">
-			<h2 className="text-xl py-4">
-				Reactry's Licences
+		<header className="bg-blue-600 text-white px-4 py-6 text-center">
+			<a className="text-sm bg-blue-400 inline-block px-4 py-2 mb-4 rounded" href="">
+				Reactry's
+			</a>
+			<h2 className="text-xl">
+				 Licence Generator
 			</h2>
 		</header>
 	);
